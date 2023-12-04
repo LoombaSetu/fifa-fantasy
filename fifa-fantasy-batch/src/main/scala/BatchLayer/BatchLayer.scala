a79fa62026e6b3f2ff203ae0c88f7f4eea04ff64
LoombaSetu <setu@uchicago.edu>
2023-12-03 20:59:04 -0600

    batch layer| hive tables added

--- fifa-fantasy-batch/src/main/scala/BatchLayer/BatchLayer.scala
+++ fifa-fantasy-batch/src/main/scala/BatchLayer/BatchLayer.scala
@@ -7,12 +7,11 @@ import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
 import org.apache.spark.ml.regression.DecisionTreeRegressor
 import org.apache.spark.ml.evaluation.RegressionEvaluator
 
-object scala {
+object ScalaBatchLayer {
   def main(args: Array[String]) {
     val conf = new SparkConf()
       .setAppName("Build FIFA Players Table")
       .setMaster("local[2]")
-
 
     val spark = SparkSession.builder()
       .appName("FIFA Players Table")
@@ -20,70 +19,75 @@ object scala {
       .enableHiveSupport()
       .getOrCreate()
 
-    val PlayerData: DataFrame = spark.read.format("csv")
-      .option("header", "true")
-      .load("hdfs:///setu-project/male_players.csv")
+    try {
+      // Read data from CSV file
+      val playerData = spark.sql("SELECT * FROM playerdatahive")
+      //val inputPlayerData = Read from Kafka topic
+
+      // Feature selection
+      val selectedFeatures = Array(
+        "pace", "shooting", "passing", "dribbling", "defending", "physic", "club_name",
+        "height_cm", "nationality_name", "preferred_foot", "player_positions"
+      )
+
+      // Select relevant features and target variables
+      val data = playerData.select((selectedFeatures.map(col) :+ col("value_eur")): _*)
+
+      // StringIndexer for categorical variables
+      val categoricalCols = Array("nationality_name", "preferred_foot", "player_positions", "club_name")
+      val indexers = categoricalCols.map(col => new StringIndexer().setInputCol(col).setOutputCol(col + "_index"))
+
+      // Assemble features into a vector
+      val assembler = new VectorAssembler()
+        .setInputCols((selectedFeatures ++ categoricalCols.map(_ + "_index")).distinct)
+        .setOutputCol("features")
+
+      // Decision Tree Regression Model
+      val dt = new DecisionTreeRegressor()
+        .setLabelCol("overall") // Change this to "value_eur" for different target variables
+        .setFeaturesCol("features")
+
+      // Create a pipeline with stages
+      val pipeline = new Pipeline().setStages(indexers ++ Array(assembler, dt))
+
+      // Split the data into training and testing sets
+      val dataArray = data.randomSplit(Array(0.8, 0.2))
+      val Array(trainingData, testData) = dataArray
+
+      // Train the model
+      val model = pipeline.fit(trainingData)
+
+      // Make predictions on the test set
+      val predictions = model.transform(testData)
+      // val inputPredictions = model.transform(inputPlayerData)
+
+      // Evaluate the model
+      val evaluator = new RegressionEvaluator()
+        .setLabelCol("value_eur")
+        .setPredictionCol("prediction")
+        .setMetricName("rmse")
+
+      val rmse = evaluator.evaluate(predictions)
+      println(s"Root Mean Squared Error (RMSE) on test set: $rmse")
+
+      // Save the model
+      model.write.overwrite().save("hdfs:///setu-project/")
+
+      // Creating views and saving to Hbase via Hive
+      spark.sql("insert overwrite table playerdataprocessed " +
+        "select concat(short_name, fifa_version)," +
+        "  fifa_version, short_name, long_name, height_cm," +
+        "  player_positions, nationality_name, age, club_name," +
+        "  preferred_foot, overall, pace, shooting, passing, dribbling, defending, physic," +
+        "  value_eur from playerdatahive;")
 
 
-//    val longPlayerData = PlayerData.selectExpr(
-//      "player_id", "fifa_version", "short_name", "long_name", "player_positions", "height_cm",
-//      "nationality_name", "preferred_foot",
-//      "stack(9, 'overall', overall, 'pace', pace, 'shooting', shooting, 'passing', passing, 'dribbling', dribbling," +
-//        " 'defending', defending, 'value_eur', value_eur, 'wage_eur', wage_eur, 'physic', physic) as (Attribute, Value)"
-//    )
-
-    // Spark ML bit
-
-    val selectedFeatures = Array(
-      "pace", "shooting", "passing", "dribbling", "defending", "physic",
-      "height_cm", "nationality_name", "preferred_foot", "player_positions"
-    )
-
-    // Select relevant features and target variables
-    val data = PlayerData.select((selectedFeatures.map(col) :+ col("overall") :+ col("value_eur") :+ col("wage_eur")): _*)
-
-
-    // StringIndexer for categorical variables
-    val categoricalCols = Array("nationality_name", "preferred_foot", "player_positions")
-    val indexers = categoricalCols.map(col => new StringIndexer().setInputCol(col).setOutputCol(col + "_index"))
-
-    // Assemble features into a vector
-    val assembler = new VectorAssembler()
-      .setInputCols((selectedFeatures ++ categoricalCols.map(_ + "_index")).distinct)
-      .setOutputCol("features")
-
-    // Decision Tree Regression Model
-    val dt = new DecisionTreeRegressor()
-      .setLabelCol("overall") // Change this to "value_eur" or "wage_eur" for different target variables
-      .setFeaturesCol("features")
-
-    // Create a pipeline with stages
-    val pipeline = new Pipeline().setStages(indexers ++ Array(assembler, dt))
-
-    // Split the data into training and testing sets
-    val dataArray = data.randomSplit(Array(0.8, 0.2))
-    val Array(trainingData, testData) = dataArray
-
-    // Train the model
-    val model = pipeline.fit(trainingData)
-
-    // Make predictions on the test set
-    val predictions = model.transform(testData)
-
-    // Evaluate the model
-    val evaluator = new RegressionEvaluator()
-      .setLabelCol("overall") // Change this to "value_eur" or "wage_eur" for different target variables
-      .setPredictionCol("prediction")
-      .setMetricName("rmse")
-
-    val rmse = evaluator.evaluate(predictions)
-    println(s"Root Mean Squared Error (RMSE) on test set: $rmse")
-
-    // Save the model
-    model.write.overwrite().save("hdfs:///setu-project/")
-
-
-
+    } catch {
+      case e: Exception =>
+        // Log or handle the exception
+        e.printStackTrace()
+    } finally {
+      spark.stop()
+    }
   }
-
 }
